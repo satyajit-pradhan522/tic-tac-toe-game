7ac902fde5f60414781cad165088f440a4170541
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-09-01 17:54:05 +0530

    Add files via upload

---
+++ Tic-Tac-Toe-Game/app.js
@@ -0,0 +1,89 @@
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+let turnO = true;//playerO, playerX
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    boxenble();
+    msgContainer.classList.add("hide");
+} 
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was click");
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        }else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const boxDisabled = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const boxenble = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner} `;
+    msgContainer.classList.remove("hide");
+    boxDisabled(); 
+}; 
+
+
+
+const checkWinner = () => {
+    let hasWin = false;
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+                hasWin = true;
+                return;
+            }
+        }
+    }
+    if (!hasWin) {
+        const allBoxes = [...boxes].every((box) => box.innerText !== "");
+        if (allBoxes) {
+            msgContainer.classList.remove('hide');
+            msg.innerText = 'Match Drawn';
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
